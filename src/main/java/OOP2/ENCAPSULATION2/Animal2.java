89304e474555ed56328a77a386a96cf9b3cf4723
shafiqct2009 <167397108+shafiqct2009@users.noreply.github.com>
2024-05-23 20:29:03 -0400

    Pushing from local

---
+++ src/main/java/OOP2/ENCAPSULATION2/Animal2.java
@@ -0,0 +1,10 @@
+package OOP2.ENCAPSULATION2;
+
+public class Animal2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/OOP2/ENCAPSULATION2/Cat2.java
@@ -0,0 +1,13 @@
+package OOP2.ENCAPSULATION2;
+
+/**
+ * Hello world!
+ *
+ */
+public class Cat2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
